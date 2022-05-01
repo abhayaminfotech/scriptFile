5d25fe7089413867e394a04ba49c78e34f00fc58
Abhayam Solution <104451380+abhayaminfotech@users.noreply.github.com>
2022-05-01 22:59:58 +0530

    Update and rename downloadScript.sh to moveFiles.sh

---
+++ moveFiles.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+
+#!/bin/bash
+
+OS_TYPE=""
+case "$(uname -s)" in
+ Darwin)
+OS_TYPE='MacOS'
+;;
+Linux)
+OS_TYPE='Linux'
+;;
+CYGWIN*|MINGW32*|MSYS*|MINGW*)
+OS_TYPE='Windows'
+;;
+*)
+echo 'Other OS'
+;;
+esac
+
+if [ "$OS_TYPE"=='Linux' ]; then
+   mv $PWD/myscripts $HOME/.buildConfig/configFile
+elif [ "$OS_TYPE"=='Windows' ] ; then
+   echo "Windows"
+elif [ "$OS_TYPE"=='MacOS' ] ; then
+   mv $PWD/myscripts $HOME/.buildConfig/configFile
+else
+    echo "Not Support"
+fi
+echo  "MOVE_SUCCESS_FILE_GET";
